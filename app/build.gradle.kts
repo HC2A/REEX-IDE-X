20d2361a6261ad4c09500d601d7cc0b65a21ca84
Ahmed <www.sudan.com.249@gmail.com>
2026-09-18 19:06:14 +0300

    Release REEX IDE X final editor version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.reex.idex"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0-alpha02"
+        versionCode = 3
+        versionName = "1.0.0-final"
     }
 
     buildTypes {
