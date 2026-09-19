519ae46a9422d33fa63cf4773f23ea9aea7821ff
Ahmed <www.sudan.com.249@gmail.com>
2026-09-19 07:37:23 +0300

    fix: opt into Material3 experimental top app bar API

--- app/src/main/java/com/reex/idex/MainActivity.kt
+++ app/src/main/java/com/reex/idex/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -86,6 +87,7 @@ class ReexApp extends StatelessWidget {
 }
 """
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ReexIdeScreen(
     activity: MainActivity,
